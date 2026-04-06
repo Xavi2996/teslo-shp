8a5de8c6bff7057c901d1b6105ebfa8f55980991
xavier <xavier.cobos.29@hotmail.com>
2026-04-05 20:14:31 -0500

    feat: integración de fuente y navBar

--- src/app/store-front/components/front-nav-bar/front-nav-bar.component.ts
+++ src/app/store-front/components/front-nav-bar/front-nav-bar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'front-nav-bar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './front-nav-bar.component.html',
   styleUrl: './front-nav-bar.component.css',
 })
